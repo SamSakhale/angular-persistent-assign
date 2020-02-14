cf65033e31adc4b9d32634088ec53f1040239e10
SamSakhale <sakhale.s@gmail.com>
2020-02-14 03:57:59 +0000

    3 files modified, 2 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ConfigService } from './config.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  logsData: any = [];
+
+  constructor(private config: ConfigService) { }
+
+  ngOnInit() {
+    this.config.getData()
+      .subscribe(data => {
+        console.log(data);
+        this.logsData = data;
+      });
+  }
 }
